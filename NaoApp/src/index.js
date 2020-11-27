6e0fbda9838262c8008d1d64ea40a53aed60cac5
Tzlil Gavra <gavrastg@gmail.com>
2020-11-27 11:49:23 +0200

    New NaoApp directory for the web part

---
+++ NaoApp/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import Quiz from "./Quiz";
+
+ReactDOM.render(<Quiz/>, document.getElementById("root"));
