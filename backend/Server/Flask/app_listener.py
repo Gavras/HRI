0d55f537b3d81f0dec7343770374ab937f492ea1
Tzlil Gavra <gavrastg@gmail.com>
2020-12-08 09:56:19 +0200

    [#6] App-Backend Integration -  Changed the backend according to `messaging_api`

--- backend/Server/Flask/app_listener.py
+++ backend/Server/Flask/app_listener.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, request
 from quiz_manager import QuizManager
 
@@ -11,28 +12,25 @@ manager = QuizManager()
 @app.route('/get_question', methods=['POST', 'GET'])
 def get_question():
     print('backend got get_question request!')
-    return manager.get_question()
-
-
-@app.route('/next_question', methods=['POST', 'GET'])
-def next_question():
-    print('backend got next_question request!')
-    manager.next_question()
-    return "Next question is now active!"
+    return create_response(manager.get_question())
 
 
 @app.route('/submit_answer', methods=['POST', 'GET'])
 def submit_answer():
     answer = request.form.get('answer')
     print('backend got submit_answer request!')
-    return manager.submit_answer(answer)
+    return create_response(manager.submit_answer(answer))
 
 
 @app.route('/get_hint', methods=['POST', 'GET'])
 def get_hint():
     print('backend got get_hint request!')
-    return manager.get_hint()
+    return create_response(manager.get_hint())
 
+def create_response(msg):
+    resp = flask.make_response(msg)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 def flask_main():
     app.run(host='0.0.0.0', port=manager.backend_port)
