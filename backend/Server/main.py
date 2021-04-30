0682bcf20156bad172b0270d8faa2df97c445333
Tzlil Gavra <gavrastg@gmail.com>
2021-04-30 20:58:19 +0300

    cleaning

--- backend/Server/main.py
+++ backend/Server/main.py
@@ -1,33 +1,45 @@
-import subprocess
-import os
-import sys
-import time
-import traceback
+import flask
+from flask import Flask, request
+from quiz_manager import QuizManager
+
+# main Flask app
+app = Flask(__name__)
+
+# HRI quiz manager
+manager = QuizManager()
+
+@app.route('/get_question', methods=['POST', 'GET'])
+def get_question():
+    if 'idx' in request.args:
+        idx = request.args.get('idx')
+    else:
+        idx = None
+    idx_text = f' idx={idx}' if idx != None else ''
+    print(f'backend got get_question request!{idx_text}')
+    return create_response(manager.get_question(idx))
 
 
-def main():
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    flask_app_path = os.path.join(current_path, 'Flask', 'app_listener.py')
-    # open a subprocess to run the flask app listener
-    print(f'Running command "{sys.executable}" "{flask_app_path}"')
-    flask_app = subprocess.Popen(f'"{sys.executable}" "{flask_app_path}"', shell=True)
-    try:
-        # add more logic/workers here
-
-        # this line will block and wait for the flask_app to finish.
-        # add this wait() method to every subprocess opened for clean exit.
-        flask_app.wait()
-    except KeyboardInterrupt:
-        print('backend caught abort signal!')
-        return 0
-    except Exception as e:
-        # print stacktrace for unknown exceptions. Handle all known exceptions before this clause
-        print('backend caught an unknown exception!')
-        print('--------- STACK TRACE ---------')
-        traceback.print_stack(e)
-        print('--------- EXITING ---------')
-        return -1
+@app.route('/submit_answer', methods=['POST', 'GET'])
+def submit_answer():
+    answer = request.args.get('answer')
+    print(f'backend got submit_answer request! answer={answer}')
+    return create_response(manager.submit_answer(answer))
 
 
-if __name__ == "__main__":
-    main()
+@app.route('/get_hint', methods=['POST', 'GET'])
+def get_hint():
+    print('backend got get_hint request!')
+    return create_response(manager.get_hint())
+
+def create_response(msg):
+    msg = flask.jsonify(msg)
+    resp = flask.make_response(msg)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+def flask_main():
+    app.run(host='0.0.0.0', port=manager.backend_port)
+
+
+if __name__ == '__main__':
+    flask_main()
