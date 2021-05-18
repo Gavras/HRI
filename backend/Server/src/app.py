84eaa4976fac062dd41df080c38e06cc828400bc
Gavras <gavrastg@gmail.com>
2021-05-18 17:55:42 +0300

    Brain - Brain script

--- backend/Server/src/app.py
+++ backend/Server/src/app.py
@@ -14,13 +14,14 @@ manager = QuizManager()
 def index():
     return 'index'
 
+
 @app.route('/get_question', methods=['POST', 'GET'])
 def get_question():
     if 'idx' in request.args:
         idx = request.args.get('idx')
     else:
         idx = None
-    idx_text = f' idx={idx}' if idx != None else ''
+    idx_text = f' idx={idx}' if idx is not None else ''
     print(f'backend got get_question request!{idx_text}')
     return create_response(manager.get_question(idx))
 
@@ -37,14 +38,17 @@ def get_hint():
     print('backend got get_hint request!')
     return create_response(manager.get_hint())
 
+
 def create_response(msg):
     msg = flask.jsonify(msg)
     resp = flask.make_response(msg)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
+
 def flask_main():
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', manager.backend_port)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', manager.server_port)))
+
 
 if __name__ == '__main__':
     flask_main()
